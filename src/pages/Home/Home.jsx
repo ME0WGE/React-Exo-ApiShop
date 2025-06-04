a54f2d681f9b98f3d643aed956158556bd205760
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-04 20:50:37 +0200

    add api call to Home and comments with the content structure

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
 import "./Home.css";
+import axios from "axios";
 
 export default function Home() {
-  return;
+  // Home (Page home avec un carousel de vos produits et une section avec les produits qui ont un rating au dessus de 4)
+  const [products, setProducts] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Call API
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("https://fakestoreapi.com/products/")
+      .then((response) => {
+        setProducts(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(`Error: ${error.message}`);
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      {/* CAROUSEL */}
+      <div className="home-container">{/* CAROUSEL */}</div>
+      {/* LOADING */}
+      {/* ERROR */}
+      {/* PRODUCTS RATED > 4* */}
+    </>
+  );
 }
