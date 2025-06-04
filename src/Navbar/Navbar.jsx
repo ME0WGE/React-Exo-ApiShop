a9a7beeee9525bbed2963fa350ea0e0285ba783f
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-04 20:32:45 +0200

    add basic navbar & footer

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
-  return;
+  return (
+    <nav className="navbar">
+      <Link to="/">
+        <h1 className="navbar-title">
+          <span className="span-love">Love</span>Shop
+        </h1>
+      </Link>
+      <Link to="/products">Products</Link>
+      {/* <FontAwesomeIcon icon={faCartShopping} className="navbar-cart" /> */}
+    </nav>
+  );
 }
