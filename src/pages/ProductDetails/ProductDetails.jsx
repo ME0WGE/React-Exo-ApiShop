9f231df5c68e92848aea7e54ff7eb99b5881f5c1
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-11 14:30:26 +0200

    add api call & basic states & params for the id

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,5 +1,33 @@
+import axios from "axios";
 import "./ProductDetails.css";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 export default function ProductDetails() {
-  return;
+  const { id } = useParams();
+  const [quantity, setQuantity] = useState(1);
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Call API
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`https://fakestoreapi.com/products/${id}`)
+      .then((response) => {
+        setProduct(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(`Error: ${error.message}`);
+        setError(error.message);
+        setLoading(false);
+      });
+  }, [id]);
+  return (
+    <>
+      <></>
+    </>
+  );
 }
